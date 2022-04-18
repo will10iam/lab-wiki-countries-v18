bd519949d5c9676b56028e0087e799f4819259e5
William Berbet <wiiliamb@hotmail.com>
2022-04-18 09:16:24 -0300

    lab incompleted

---
+++ src/Components/CountriesList/index.js
@@ -0,0 +1,11 @@
+export function CountriesList() {
+    return ( 
+        <div>
+            <div>
+              <Link to="alpha3Code">🇦🇼 Aruba</Link>
+            </div>
+        </div>
+     );
+}
+
+export default CountriesList;
---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,7 @@
+export function NavBar() {
+    return ( 
+        <h1 className="navbar navbar-light bg-primary mb-3">LAB-WikiCountries</h1>
+     );
+}
+
+export default NavBar;
